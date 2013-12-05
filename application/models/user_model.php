199b05b2df0c48462d3f2801ba8c2132b7cd9351
Windcode <windcode.fr@gmail.com>
2013-12-06 00:27:27 +0100

    [Updated] User model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,46 +1,45 @@
-<?php 
+<?php
 
-Class User_model extends Models{
+Class User_model extends CI_Model
+{
 
-	function User_model()
-	{
-		//Call the Model constructor
-		parent::Model();
-	}
+    function User_model()
+    {
+        //Call the Model constructor
+        parent::__construct();
+    }
 
-	function add($data)
-	{
-		$this->db->insert('user', $data);
-	}
+    function add($data)
+    {
+        $this->db->insert('user', $data);
+    }
 
-	function update($data, $pseudo)
-	{
-		$this->db->where('pseudo', $pseudo);
-		$this->db->update('user', $data);
-	}
+    function update($data, $pseudo)
+    {
+        $this->db->where('pseudo', $pseudo);
+        $this->db->update('user', $data);
+    }
 
-	function delete($pseudo)
-	{
-		$this->db->where('pseudo', $pseudo);
-		$this->db->delete('user');
-	}
+    function delete($pseudo)
+    {
+        $this->db->where('pseudo', $pseudo);
+        $this->db->delete('user');
+    }
 
-	function get($pseudo)
-	{
-		$this->db->where('pseudo', $pseudo);
-		$query = $this->db->select('user');
-		return $query;
-	}
+    function get($pseudo)
+    {
+        $this->db->where('pseudo', $pseudo);
+        $query = $this->db->select('user');
+        return $query;
+    }
 
-	function count()
-	{
-		return $this->db->count_all('user');
-	}
+    function count()
+    {
+        return $this->db->count_all('user');
+    }
 
-	function getAll()
-	{
-		return $this->db->get();
-	}
+    function getAll()
+    {
+        return $this->db->get();
+    }
 }
-
- ?>
