12834d19d19fc173411b636020b5f7f7a3de7e2f
Windcode <windcode.fr@gmail.com>
2013-12-06 02:07:08 +0100

    [Added] User model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,44 +2,88 @@
 
 Class User_model extends CI_Model
 {
+    // Table name
+    private $table = 'user';
 
+    /**
+     * Construct
+     */
     function User_model()
     {
         //Call the Model constructor
         parent::__construct();
     }
 
-    function add($data)
+    /**
+     * Insert a new User in DB
+     *
+     * @param array $data Array that contains all user data
+     */
+    function insert($data)
     {
         $this->db->insert('user', $data);
+        $data['password'] = hash('sha256', $data['password']);
+        return $this->db->insert_id();
     }
 
-    function update($data, $pseudo)
+    /**
+     * Update a user, password will be hashed
+     *
+     * @param string $username Username
+     * @param array $data Data for update
+     */
+    public function update($username, $data)
     {
-        $this->db->where('pseudo', $pseudo);
-        $this->db->update('user', $data);
+        // Prevent overwriting with a blank password
+        if (isset($data['password']) && $data['password']) {
+            $data['password'] = hash('sha256', $data['password']);
+        } else {
+            unset($data['password']);
+        }
+
+        $this->db->where('username', $username)->update($this->table, $data);
     }
 
-    function delete($pseudo)
+
+    /**
+     * Delete a user
+     *
+     * @param string $where identification field
+     * @param bool $value
+     * @internal param \value $int
+     */
+    public function delete($where, $value = false)
     {
-        $this->db->where('pseudo', $pseudo);
-        $this->db->delete('user');
+        if (!$value) {
+            $value = $where;
+            $where = 'username';
+        }
+
+        $this->db->where($where, $value)->delete($this->table);
     }
 
     function get($pseudo)
     {
-        $this->db->where('pseudo', $pseudo);
-        $query = $this->db->select('user');
-        return $query;
+        return $this->db->where("username", $pseudo)->get($this->table)->row_array();
     }
 
-    function count()
+    /**
+     * Count number of registered users
+     * @return Number of registered users
+     */
+    public function count()
     {
-        return $this->db->count_all('user');
+        return $this->db->count_all_results($this->table);
     }
 
+    /**
+     * TODO
+     *
+     * @return mixed
+     */
     function getAll()
     {
         return $this->db->get();
     }
+
 }
